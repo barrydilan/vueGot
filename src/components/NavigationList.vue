b3050cf4a7659cad679931459f578cf604393b39
barrydillan <queharambe@gmail.com>
2024-01-12 12:05:15 +0100

    feat: delete extra files

---
+++ src/components/NavigationList.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav>
+    <ul class="nav_list">
+      <li class="nav_list-item">Houses</li>
+      <li class="nav_list-item">Persons</li>
+      <li class="nav_list-item">Quotes</li>
+    </ul>
+  </nav>
+</template>
+
+<style>
+nav {
+  width: 100%;
+  font-size: 12px;
+  text-align: center;
+  margin-top: 2rem;
+}
+
+.nav_list {
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
+}
+</style>
