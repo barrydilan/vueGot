d36528df4a9c80000e57895642e6faa598ad7df6
barrydillan <queharambe@gmail.com>
2024-01-17 16:14:57 +0100

    feat: main layout responsivness

--- src/components/SearchResults.vue
+++ src/components/SearchResults.vue
@@ -16,4 +16,11 @@ ul {
   grid-template-columns: repeat(2, 1fr);
   gap: 0.6rem;
 }
+
+@media (max-width: 968px) {
+  ul {
+    grid-template-columns: 1fr;
+  }
+}
+
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -23,9 +23,11 @@ import QuotesView from './QuotesView.vue'
   display: grid;
   gap: 2rem;
   grid-template-columns: repeat(3, 1fr);
+  max-width: 1200px;
+  margin: 0 auto;
 }
 
-@media (max-width: 420px) {
+@media (max-width: 580px) {
   .main {
     display: grid;
     gap: 2rem;
